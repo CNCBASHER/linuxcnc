3c5c18a0a1ef02ebf1ea74dc70d8ddbb440ab95e
Yishin Li <ysli@araisrobo.com>
2012-04-17 06:27:16 +0800

    Update binary release revision -- 2012.04.16
    * lco2_00 ... is no allowed as changelog revision name

--- debian/build.sh
+++ debian/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #usage: source ./build.sh
-RELEASE=2.4.0-nurbs-2011.08.31
+RELEASE=2.4.0-nurbs-2012.04.16
 ./configure sim
 nice debuild -S
 sudo nice pbuilder build ../../emc2_${RELEASE}.dsc
 cp -v \
   /var/cache/pbuilder/result/emc2-sim_${RELEASE}_i386.deb \
+  /var/cache/pbuilder/result/emc2-sim-dev_${RELEASE}_i386.deb \
   ~/tmp
